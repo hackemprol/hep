962cb6af32b1b6ccff4c9c4bc4ecffe21514baa6
Lucian Fialho <lucianfialhobp@bitbucket.org>
2015-02-27 00:53:12 -0300

    -- home

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+var hep = {
+	init: function(){
+		this.backstrech();
+	},
+	backstrech: function(){
+		$.backstretch("assets/images/bg.jpg");
+	}
+};
+(function($, undefined){
+	hep.init();
+})(jQuery);
